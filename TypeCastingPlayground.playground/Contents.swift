29e1030a7368ac233b739c9403c876a1b8a658a8
wind0ws <932803995@qq.com>
2016-08-22 15:56:17 +0800

    Add Any AnyObject sample function

--- TypeCastingPlayground.playground/Contents.swift
+++ TypeCastingPlayground.playground/Contents.swift
@@ -69,14 +69,87 @@ func testAsIs() -> Void {
     print("强制转换Song \(song) ")
 }
 
-testAsIs()
+//testAsIs()
 
 
 /*
- 
+ 不确定类型
  Any  和  AnyObject
  
- Any  真的是Any，包罗万象
+ Any        真的是Any，包罗万象。所有类型都可以
  AnyObject  面向对象嘛，AnyObject只能放class类型的，任何class类型都可
  
+ 注意：只有你明确需要它的行为使用和功能才使用Any和AnyObject.通常使用明确的类型总是更好的。
+ （将白了，就是不要滥用Any和AnyObject）
+ 
  */
+
+func testAnys() -> Void {
+//    var anys = [Any]()
+    var anys:[Any] = [
+        12,
+        0,
+        "Hello,Swift",
+        MediaItem(name: "QingHuaCi"),
+        Song(artiest: "ZhouJieLun", name: "ShuangJieGun"),
+        Movie(director: "ZhouXingChi", name: "Kongfu"),
+        (404,"Not Found")
+    ]
+    anys.append(){
+        (name:String,age:Int) -> String in
+        return "\(name) : \(age)"
+    }
+    let tuple = (10,11)
+    anys.insert(tuple, atIndex: 0)
+    anys.insert(0.0, atIndex: 2)
+    
+    
+    for element in anys {
+        switch element {
+        case 0 as Int:
+            print("This is Int Zero")
+        case 0 as Double:
+            print("This is Double Zero")
+        case let someInt as Int:
+            print("This is Int \(someInt)")
+        case let someStr as String:
+            print("This is String \(someStr)")
+        case let someMediaItem as MediaItem:
+            print("\(someMediaItem)")
+        case let (x,y) as (Int,Int):
+            print("This is point:(\(x),\(y))")
+        case let (errorCode,message) as (Int,String):
+            print("This is request result,(\(errorCode),\(message))")
+        case let function as (String,Int) ->String:
+            print("This is function,\(function("ZhangSan",18))")
+        default:
+            print("Default branch")
+        }
+    }
+    
+}
+
+//testAnys()
+
+
+func testAnyObjects() -> Void {
+    var things = [AnyObject]()
+    things.append(MediaItem(name: "DongFengPo"))
+    things.append(Movie(director: "ZhouXingChi", name: "MeiRenYu"))
+    things.append(Song(artiest: "ZhouJieLun", name: "QingHuaCi"))
+    
+    if let medias = things as? [MediaItem] {
+        for media in medias {
+            switch media {
+            case let song as Song:
+                print("This is Song,arties is \(song.artiest),name is \(song.name)")
+            case let movie as Movie:
+                print("This is Movie,director is \(movie.director),name is \(movie.name)")
+            default:
+                print("This is normal MediaItem ,name is \(media.name)")
+            }
+        }
+    }
+}
+
+//testAnyObjects()
